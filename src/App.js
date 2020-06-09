98c3e3f1c2b0e1e1115c1de1cef67447f6bb98f2
Louie F <fitz4492@gmail.com>
2020-06-08 19:14:13 -0500

    upgraded websockets to websocket-extensions@^0.1.4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-export const App = ( ) => {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
-}
+export const App = () => (
+  <div className="App">
+    <h1>hello</h1>
+  </div>
+);
